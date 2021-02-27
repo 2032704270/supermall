776e66e43dcfbd916b174e42eaae6646e64b43e4
Tcm <2032704270@qq.com>
2021-02-28 00:26:04 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
+    <main-tab-bar class="main-tab-bar"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
+import MainTabBar from 'components/content/mainTabbar/MainTabBar'
 
 export default {
   name: 'App',
   components: {
+    MainTabBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "assets/css/base.css";
+.main-tab-bar {
+  z-index: 999;
 }
 </style>
---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll';
+
+export default {
+  name: "Scroll",
+  props: {
+    probeType: {
+      type: Number,
+      default: 0
+    },
+    pullUpLoad: {
+      type: Boolean,
+      default: false
+    },
+    click: {
+      type: Boolean,
+      default: false
+    },
+  },
+  data() {
+    return {
+      scroll: null,
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      probeType: this.probeType,
+      pullUpLoad: this.pullUpLoad,
+      click: this.click,
+    })
+
+    if (this.probeType === 2 || this.probeType === 3) {
+      this.scroll.on("scroll", (position) => {
+        this.$emit("scroll", position);
+      })
+    }
+
+    if (this.pullUpLoad) {
+      this.scroll.on("pullingUp", () => {
+        this.$emit("pullingUp");
+        console.log("上拉加载更多")
+      })
+    }
+  },
+  methods: {
+    scrollTo(x, y, time=300) {
+      this.scroll.scrollTo(x, y, time);
+    },
+    finishPullUp() {
+      this.scroll.finishPullUp();
+    },
+    refresh() {
+      this.scroll.refresh();
+    },
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  overflow: hidden;
+}
+</style>
